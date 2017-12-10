71607b4f4dc0d5316a73440eeb16740ed71e7cf9
Christie <thornton.christie@gmail.com>
2017-12-09 22:40:57 -0500

    Reveal on scroll functionality added.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,27 @@
+RevealOnScroll($(".newsletter__dark-background"), "55%");
+
+
+function RevealOnScroll(els, offset) {
+        
+        var itemsToReveal = els;
+        var offsetPercentage = offset;
+        hideInitially();
+        createWaypoints();
+   
+
+    function hideInitially() {
+        itemsToReveal.addClass("waypoints");
+    } 
+
+    function createWaypoints() {        
+    
+            var waypoint = new Waypoint({
+                element: document.getElementById('newsletter-section'),
+                handler: function() {
+                    itemsToReveal.addClass("waypoints__show-item");
+                },
+                offset: offsetPercentage
+                });
+       
+    }//end createWaypoints
+ }
